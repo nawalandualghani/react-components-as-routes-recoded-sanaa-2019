3d02217765e72dfcf78b40dc1fc0a6f9b60f2d4c
nawal abdualghani <nawalabdualghani@gmail.com>
2019-12-12 08:03:05 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Home'
-import About from './About'
+import About from './about'
 import Login from './Login'
 import Navbar from './Navbar'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
